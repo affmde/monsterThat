41c3fe0e2e54882c0556285fb2fb1b5b8612c9ee
André Miranda <affmde@hotmail.com>
2022-05-26 11:26:56 +0300

    gymLeaderIntro fixed image

--- js/gymLeaderIntro.js
+++ js/gymLeaderIntro.js
@@ -4,12 +4,12 @@ class GymLeaderIntro extends Phaser.Scene{
     }
 
     preload(){
-        this.load.image('leader', 'assets/leader1.png')
+        this.load.image(`${currentOponnent.fig}`, `assets/opponents/${currentOponnent.fig}.png`)
     }
 
     create(){
         const rectBg= this.add.rectangle(0,0,w,h, 0x000000).setOrigin(0);
-        const leader= this.add.image(w*0.5, h*0.5, 'leader')
+        const leader= this.add.image(w*0.5, h*0.5, currentOponnent.fig)
             .setOrigin(0.5)
             .setScale(5)
             .setInteractive()
